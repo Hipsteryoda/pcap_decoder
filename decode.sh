f8a916099f4453d7bb91990a379fa0796fd4439f
Hipsteryoda <ksmith952913@gmail.com>
2023-01-11 13:15:52 -0700

    indentation updates

--- decode.sh
+++ decode.sh
@@ -7,25 +7,25 @@ file="${filename%.*}"
 
 # If the file exists
 if [[ -e $1 ]]
-then
-    # unzip pcap file
-    unzip $fullfile
+    then
+        # unzip pcap file
+        unzip $fullfile
 
-    # Decode pcap into text file
-    echo "Decoding pcap into text file"
-    tshark -V -r data/$file > ./pcaps/$file.txt
+        # Decode pcap into text file
+        echo "Decoding pcap into text file"
+        tshark -V -r data/$file > ./pcaps/$file.txt
 
-    # Run python decoder over file
-    echo "Analyzing and graphing stuff"
-    ./decoder.py $file.txt
+        # Run python decoder over file
+        echo "Analyzing and graphing stuff"
+        ./decoder.py $file.txt
 
-    # Remove txt file
-    echo "Cleanup time"
-    # rm -rf ./data/* ./pcaps/$filename.txt
-    
-    echo "All set"
+        # Remove txt file
+        echo "Cleanup time"
+        # rm -rf ./data/* ./pcaps/$filename.txt
+        
+        echo "All set"
 
-else
-    echo "File does not exist"
+    else
+        echo "File does not exist"
 
 fi
